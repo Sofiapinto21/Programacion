559efdb0877b0ea3e3257bf47063de91c83ee91f
Sofiapinto21 <149717153+Sofiapinto21@users.noreply.github.com>
2026-09-10 19:18:27 +0200

    Add basic Python print statements and comments

---
+++ 00_hello.py
@@ -0,0 +1,21 @@
+# Esto es un comentario
+print("Hola Python")
+print('Hola Python')
+
+"""
+Este es un
+comentario
+en varias lineas
+"""
+'''
+Este también es un
+comentario
+en varias lineas
+'''
+
+# Consultar el tipo de dato
+print(type("Soy un dato str")) # Tipo de dato: str
+print(type(5)) # Tipo de dato: int
+print(type(1.5)) # Tipo de dato: float
+print(type(1 + 2j)) # Tipo de dato: complex
+print(type(True)) # Tipo de dato: bool
